d202365686074a7b5acc864090a2e97cb889e98d
ductuantruong <ductuanpy99@gmail.com>
2022-11-02 10:07:10 +0800

    add lightning model

---
+++ Model/lightning_model.py
@@ -0,0 +1,136 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# torch.use_deterministic_algorithms(True)
+
+import pytorch_lightning as pl
+from Model.model import PairNet
+
+from utils.loss import MultiBoxLoss
+
+class LightningModel(pl.LightningModule):
+    def __init__(self, HPARAMS):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.model = PairNet(HPARAMS['n_classes'])
+            
+        self.det_criterion = MultiBoxLoss()
+        self.seg_criterion = torch.nn.CrossEntropyLoss(ignore_index=255)
+
+        self.lr = HPARAMS['lr']
+        print(f"Model Details: #Params = {self.count_total_parameters()}\t#Trainable Params = {self.count_trainable_parameters()}")
+
+    def count_total_parameters(self):
+        return sum(p.numel() for p in self.parameters())
+    
+    def count_trainable_parameters(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.lr)
+        return [optimizer]
+
+    def training_step(self, batch, batch_idx):
+        img, bboxes, det_labels, seg_labels = batch
+        y_h = torch.stack(y_h).reshape(-1,)
+        y_a = torch.stack(y_a).reshape(-1,)
+        y_g = torch.stack(y_g).reshape(-1,)
+        
+        loc_hat, det_hat, seg_hat = self(img)
+
+        loc_loss, det_loss = self.det_criterion(loc_hat, det_hat, bboxes, det_labels)
+        seg_loss = self.seg_criterion(seg_hat, seg_labels)
+        loss = loc_loss + det_loss + seg_loss
+
+        return {
+                'loss':loss, 
+                'train_loc_loss': det_loss.item(),
+                'train_det_loss': det_loss.item(),
+                'train_seg_loss': seg_loss.item(),
+            }
+    
+    def training_epoch_end(self, outputs):
+        n_batch = len(outputs)
+        loss = torch.tensor([x['loss'] for x in outputs]).mean()
+        loc_loss = torch.tensor([x['train_loc_loss'] for x in outputs]).sum()/n_batch
+        det_loss = torch.tensor([x['train_det_loss'] for x in outputs]).sum()/n_batch
+        seg_loss = torch.tensor([x['train_seg_loss'] for x in outputs]).sum()/n_batch
+
+        self.log('train/loss' , loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('train/loc', loc_loss.item(), on_step=False, on_epoch=True, prog_bar=True)
+        self.log('train/det', det_loss.item(), on_step=False, on_epoch=True, prog_bar=True)
+        self.log('train/seg', seg_loss.item(), on_step=False, on_epoch=True, prog_bar=True)
+
+    def validation_step(self, batch, batch_idx):
+        img, bboxes, det_labels, seg_labels = batch
+
+        loc_hat, det_hat, seg_hat = self(img)
+
+        seg_loss = self.seg_criterion(seg_hat, seg_labels)
+        loc_loss, det_loss = self.det_criterion(loc_hat, det_hat, bboxes, det_labels)
+        val_loss = loc_loss + det_loss + seg_loss
+
+        return {
+                'val_loss':val_loss, 
+                'val_loc_loss':loc_loss.item(),
+                'val_det_loss':det_loss.item(),
+                'val_seg_loss':seg_loss.item()
+            }
+
+    def validation_epoch_end(self, outputs):
+        n_batch = len(outputs)
+        val_loss = torch.tensor([x['val_loss'] for x in outputs]).mean()
+        loc_loss = torch.tensor([x['val_loc_loss'] for x in outputs]).sum()/n_batch
+        det_loss = torch.tensor([x['val_det_loss'] for x in outputs]).sum()/n_batch
+        seg_loss = torch.tensor([x['val_seg_loss'] for x in outputs]).sum()/n_batch
+
+        self.log('val/loss' , val_loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('val/loc', loc_loss.item(), on_step=False, on_epoch=True, prog_bar=True)
+        self.log('val/det', det_loss.item(), on_step=False, on_epoch=True, prog_bar=True)
+        self.log('val/seg', seg_loss.item(), on_step=False, on_epoch=True, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        img, bboxes, det_labels, seg_labels = batch
+        y_h = torch.stack(y_h).reshape(-1,)
+        y_a = torch.stack(y_a).reshape(-1,)
+        y_g = torch.stack(y_g).reshape(-1,)
+        
+        loc_hat, det_hat, seg_hat = self(img)
+
+        loc_loss, det_loss = self.det_criterion(loc_hat, det_hat, bboxes, det_labels)
+        seg_loss = self.seg_criterion(seg_hat, seg_labels)
+        loss = loc_loss + det_loss + seg_loss
+
+        return {
+                'test_loss':loss, 
+                'test_loc_loss':loc_loss.item(),
+                'test_det_loss':det_loss.item(),
+                'test_seg_loss':seg_loss.item()
+            }
+
+
+    def test_epoch_end(self, outputs):
+        n_batch = len(outputs)
+        test_loss = torch.tensor([x['test_loss'] for x in outputs]).mean()
+        loc_loss = torch.tensor([x['test_loc_loss'] for x in outputs]).sum()/n_batch
+        det_loss = torch.tensor([x['test_det_loss'] for x in outputs]).sum()/n_batch
+        seg_loss = torch.tensor([x['test_seg_loss'] for x in outputs]).sum()/n_batch
+
+        self.log('test/loss', test_loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('test/loc', loc_loss.item(), on_step=False, on_epoch=True, prog_bar=True)
+        self.log('test/det', det_loss.item(), on_step=False, on_epoch=True, prog_bar=True)
+        self.log('test/seg', seg_loss.item(), on_step=False, on_epoch=True, prog_bar=True)
+
+
+        pbar = {
+                'test/loss':test_loss.item(),
+                'test/loc':loc_loss.item(),
+                'test/det':det_loss.item(),
+                'test/seg':seg_loss.item()
+            }
+        self.logger.log_hyperparams(pbar)
+        self.log_dict(pbar)
